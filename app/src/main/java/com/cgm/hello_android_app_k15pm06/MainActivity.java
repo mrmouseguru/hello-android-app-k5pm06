5f955971eff504e0012b1fd0935ce61c24371626
CGM <mrmouse.guru@gmail.com>
2024-03-09 08:21:15 +0700

    initialize

---
+++ app/src/main/java/com/cgm/hello_android_app_k15pm06/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cgm.hello_android_app_k15pm06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
