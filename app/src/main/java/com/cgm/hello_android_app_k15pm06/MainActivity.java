1c93fdb2899def608e721f0653ee9566a1c06b6e
CGM <mrmouse.guru@gmail.com>
2024-03-09 08:44:32 +0700

    put product list data into ListView

--- app/src/main/java/com/cgm/hello_android_app_k15pm06/MainActivity.java
+++ app/src/main/java/com/cgm/hello_android_app_k15pm06/MainActivity.java
@@ -3,6 +3,14 @@ package com.cgm.hello_android_app_k15pm06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.cgm.hello_android_app_k15pm06.entities.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.productListView);
+
+        List<Product> productListData = Arrays.asList(
+                new Product("Sam sung galaxy s 21", 1500, "ss.jpg"),
+                new Product("Iphone 15 promax", 2000, "ip.jpg")
+        );
+
+        ArrayAdapter<Product> adapter =
+                new ArrayAdapter<Product>
+                        (this, android.R.layout.simple_list_item_1, productListData);
+        listView.setAdapter(adapter);
     }
 }
